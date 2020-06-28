f42540839bd017c2ef5c318d539fdd5693833b93
colemiller21 <cole.t.miller@gmail.com>
2020-06-27 19:29:15 -0500

    updated backend, configured babel in node

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,32 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyparser = require("body-parser");
-const cors = require("cors");
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyparser from 'body-parser';
+import cors from 'cors';
 
 
-const app = express();
+
+const app = express()
 const PORT = 3001;
 
 //mongo connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/green2greenDB', {
+mongoose.connect('mongodb://localhost/soccerDB', {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
 });
 
 //body parser setup
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 
+
+//CORS setup
+
 app.use(cors());
+
+// routes(app);
 
 
 app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`)
+    console.log(`Your server is running on ${PORT}`)
 });
-
-
