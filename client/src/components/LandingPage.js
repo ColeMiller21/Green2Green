a5790de6d4dc4a090b7effc81150f9b06c3a4d91
colemiller21 <cole.t.miller@gmail.com>
2020-07-15 13:21:33 -0500

    config

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import SignInForm from './SignInForm';
 import SignUpForm from './SignUpForm';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
-import Homepage from '../components/Homepage';
+import Homepage from './Homepage';
 
 
 class LandingPage extends React.Component {
