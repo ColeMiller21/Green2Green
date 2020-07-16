ac01020e2617d08a44acdb9163c046d7ababbf9a
colemiller21 <cole.t.miller@gmail.com>
2020-07-16 16:46:05 -0500

    a

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -106,6 +106,21 @@ class Homepage extends React.Component {
 
 
     render() {
+
+        const scoreRender = this.state.scores.map((score, i) => {
+            console.log(score)
+            return (
+                <tr key={"uniqueKey" + i}>
+                    <th scope="row" style={{ width: '20%' }}>{moment(score.createdAt).format('MM-DD-YY')}</th>
+                    <td style={{ width: '10%' }}>{score.totalScore}</td>
+                    <td style={{ width: '40%' }}>{score.courseName}</td>
+                    <td style={{ width: '10%' }}>{score.frontNine}</td>
+                    <td style={{ width: '10%' }}>{score.backNine}</td>
+                </tr>
+
+            )
+        })
+
         return (
             <div>
                 <Navbar
@@ -144,21 +159,10 @@ class Homepage extends React.Component {
                             </tr>
                         </thead>
                         <tbody>
-                            {this.state.scores.length === 0 ? <h1>Insert a new score!</h1>
+                            {this.state.isLoaded ?
+                                <h1>Got User info</h1>
                                 :
-                                this.state.scores.map((score, i) => {
-                                    console.log(score)
-                                    return (
-                                        <tr key={"uniqueKey" + i}>
-                                            <th scope="row" style={{ width: '20%' }}>{moment(score.createdAt).format('MM-DD-YY')}</th>
-                                            <td style={{ width: '10%' }}>{score.totalScore}</td>
-                                            <td style={{ width: '40%' }}>{score.courseName}</td>
-                                            <td style={{ width: '10%' }}>{score.frontNine}</td>
-                                            <td style={{ width: '10%' }}>{score.backNine}</td>
-                                        </tr>
-
-                                    )
-                                })
+                                <h1>Loading.....</h1>
                             }
 
                         </tbody>
