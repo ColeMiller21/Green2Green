0673e03d4bbe31ee22b32c982feee7e05447894f
colemiller21 <cole.t.miller@gmail.com>
2020-07-16 20:24:57 -0500

    server file

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const config = require('config');
 const jwt = require('jsonwebtoken');
 const JWTSECRET = process.env.JWT_SECRET;
 
