0bd112690f196252207ffb6793c072a34197c59e
colemiller21 <cole.t.miller@gmail.com>
2020-07-16 20:32:07 -0500

    Server update

--- index.js
+++ index.js
@@ -26,9 +26,7 @@ if (process.env.NODE_ENV === 'production') {
     //set static folder
     app.use(express.static('client/build'));
 
-    // app.get('*', (req, res) => {
-    //     res.sendFile(path.resolve(__dirname, "client", "public", "index.html"))
-    // });
+
 }
 
 
@@ -48,7 +46,9 @@ app.use('/api/users', user)
 app.use('/api/auth', auth)
 
 
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "public", "index.html"))
+});
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server started on PORT ${PORT}`);
